8f65186b029ee9ad84577e7813f09827b495129e
Jérémy Hure <jeremy.hure@uha.fr>
2020-12-14 20:29:30 +0100

    #20 Execute Test Page (/execute) created and (/next) begun

--- mantest-app/src/main/java/fr/uha/ensisa/ff/mantest_app/controller/TestController.java
+++ mantest-app/src/main/java/fr/uha/ensisa/ff/mantest_app/controller/TestController.java
@@ -1,6 +1,7 @@
 package fr.uha.ensisa.ff.mantest_app.controller;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import fr.uha.ensisa.gl.turbocheese.mantest.ExecutedTest;
+import fr.uha.ensisa.gl.turbocheese.mantest.ExecutedTest.State;
+import fr.uha.ensisa.gl.turbocheese.mantest.Report;
 import fr.uha.ensisa.gl.turbocheese.mantest.Test;
 import fr.uha.ensisa.gl.turbocheese.mantest.dao.DaoFactory;
 
@@ -16,6 +20,8 @@ public class TestController {
 	
 	@Autowired
 	public DaoFactory daoFactory;
+	
+	private Report report;
 	
 	@RequestMapping(value="/")
 	public String hello() throws IOException{
@@ -53,5 +59,27 @@ public class TestController {
 		daoFactory.getTestDao().remove(deleteTest);
 		return "redirect:/list";
 	}
-
+	@RequestMapping(value="/execute")
+	public ModelAndView execute(@RequestParam(required=true) String id) throws IOException {
+		if (id=="start") {
+			//DO SOMETHING WITH REPORT
+		}
+		Iterator<Test> i = daoFactory.getTestDao().findAll().iterator();
+		ModelAndView ret = new ModelAndView("execute");
+		ret.addObject("testtoexecute",i.next());
+		return ret;
+	}
+	@RequestMapping(value="/next")
+	public String next(@RequestParam(required=true) String state, @RequestParam(required=true) String comment, @RequestParam(required=true) long id) throws IOException {
+		ExecutedTest.State s;
+		switch (state) {
+		case "success" : s=State.SUCCESS; break;
+		case "fail" : s=State.FAILED; break;
+		default : s=State.SKIPED;
+		}
+		//ExecutedTest et = new ExecutedTest(daoFactory.getTestDao().find(id), s , comment);
+		//report.addExecutedTest(et);
+		return "redirect:/list";
+	}
+	
 }
