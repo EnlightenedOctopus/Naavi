c910bfb62f38f033329b0320d645c2c847a4408d
Jérémy Hure <jeremy.hure@uha.fr>
2021-01-08 20:18:18 +0100

    Correction Test parce que avant on getReport(Date) et mtn getReport(id)

--- mantest-dao-mem/src/test/java/fr/uha/ensisa/gl/turbocheese/mantest/dao/mem/TestReportDaoMem.java
+++ mantest-dao-mem/src/test/java/fr/uha/ensisa/gl/turbocheese/mantest/dao/mem/TestReportDaoMem.java
@@ -22,7 +22,7 @@ class TestReportDaoMem {
 	@DisplayName("We can add a report in the dao")
 	void testReportDaoMemAdd() {
 		mem.addReport(r);
-		assertEquals(r, mem.getReport(r.getDate().getTime()));
+		assertEquals(r, mem.getReport(r.getId()));
 		assertEquals(mem.findAll().size(), mem.count());
 	}
 	
